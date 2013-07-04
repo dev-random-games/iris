cef558c4974164b80324343b19865cff05001b81
Lane Aasen <laneaasen@gmail.com>
2013-07-04 14:16:17 -0700

    imports slick utils

---
+++ src/main/SimpleSlickGame.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class SimpleSlickGame extends BasicGame {
+
+	public SimpleSlickGame(String gamename) {
+		super(gamename);
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+	}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {
+	}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		g.drawString("Howdy!", 100, 10);
+	}
+
+	public static void main(String[] args) {
+		try {
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new SimpleSlickGame("Simple Slick Game"));
+			appgc.setDisplayMode(640, 480, false);
+			appgc.start();
+		} catch (SlickException ex) {
+			Logger.getLogger(SimpleSlickGame.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		
+	}
+}
