6b5be40d387dbbec2872c7d962140e0cccc25a69
Dylan Swiggett <dylanswiggett@gmail.com>
2013-07-04 13:56:02 -0700

    Added KeyPress to ViewController.

--- src/main/ViewController.java
+++ src/main/ViewController.java
@@ -1,12 +1,30 @@
 package main;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
 public class ViewController implements Runnable{
 	
 	Model model;
+	
+	public enum KeyPress {
+		FORWARD(Keyboard.KEY_W),
+		BACKWARD(Keyboard.KEY_S),
+		RIGHT(Keyboard.KEY_D),
+		LEFT(Keyboard.KEY_A);
+		
+		private int keyID;
+		
+		private KeyPress(int keyID){
+			this.keyID = keyID;
+		}
+		
+		public int getKeyID(){
+			return keyID;
+		}
+	}
 	
 	public ViewController(Model model){
 		this.model = model;
